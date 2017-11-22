55efe766d8ff803f5bbb16e170797679d3467e8f
sqlpanda <po@sqlpanda.com>
2017-11-22 00:29:32 -0500

    add xmr support

--- ethmon.pl
+++ ethmon.pl
@@ -1,24 +1,54 @@
 #!/usr/bin/perl
 #use Smart::Comments '###', '####';
 use Sys::Hostname;
+use Cwd;
 use strict;
 use warnings;
 
+my $dir = getcwd;
+my $xmr= '45pwYiBt5vvWm6KgsAkGkrZztC7NthMbEMMvgUGD6SitK7YDbPgFvJS2mNiW4Fx4Pb2oncinP92A1j7uVxHdjCmKHQpPjHH';
+my $xmr_pool = 'stratum+tcp://pool.supportxmr.com:7777';
+
+
+# `pwd`/ccminer -a cryptonight -o stratum+tcp://pool.supportxmr.com:7777 -u 45pwYiBt5vvWm6KgsAkGkrZztC7NthMbEMMvgUGD6SitK7YDbPgFvJS2mNiW4Fx4Pb2oncinP92A1j7uVxHdjCmKHQpPjHH -p pandaE1 -d 0
 my $log_file = '/tmp/ethmon.log';
 _print('--------------------');
 _print("ethmon starting ...");
+my $worker = _get_worker();
+my $cpu_worker = "$worker-CPU";
+# Setting up the mem/core
+_print('Checking default core/mem');
+my $cmd1= 'ethos-readdata core > /var/run/ethos/defaultcore.file';
+my $cmd2= 'ethos-readdata mem > /var/run/ethos/defaultmem.file';
+system($cmd1);
+system($cmd2);
+# start cpuminer 
+my $has_cpu_miner = _check_cpu_miner( $cpu_worker );
+if ( $has_cpu_miner ) {
+	_print("CPU miner is health ...");
+}
+else {
+	_print("CPU miner is not health ...");
+	_start_cpu_miner()
+}
+# start xmr miner  if ethmon die 
+
 
 my $ref_bad_gpu= _get_gpu_state();
 my %bad_gpu = %{$ref_bad_gpu};
+my @new_miners;
 if ( scalar keys %bad_gpu  > 0 ) {
-	my $worker = _get_worker();
 	foreach my $gpu( keys %bad_gpu ) {
 		my $pid = $bad_gpu{$gpu};
 		my $return = _kill_ethminer($pid);
 		if ( $return ) {
-			_start_ethminer ($gpu,$worker);
+			push @new_miners, $gpu;
+			#_start_gpuXMR($gpu,$worker);
+			#_start_ethminer ($gpu,$worker);
 		}
 	}
+
+	_start_miner(\@new_miners);
 
 }
 else {
@@ -26,6 +56,46 @@ else {
 }
 
 	_print('--------END---------');
+
+sub _find_miner_need_eth {
+	my $c_host = hostname;
+	my @temps = qw//;
+        my $cmd = `grep sel /home/ethos/local.conf |grep -v '#' |grep $c_host`;
+        if ( $cmd) {
+		@temps = split(/\s+/,$cmd);
+		shift @temps;
+		shift @temps;
+	}
+	
+	
+	return \@temps;
+	
+}
+
+sub _start_miner{
+	my $ref_input = shift;
+	my @bad_gpus = @$ref_input;
+	my @eth_gpus = @{_find_miner_need_eth()};
+	my %eth=map{$_ =>1} @eth_gpus;
+	my %bad=map{$_=>1} @bad_gpus;
+	
+	# the intersection of @females and @simpsons:
+	my @eth_bad = grep( $bad{$_}, @eth_gpus );
+	### @eth_bad		
+	if ( scalar @eth_bad > 0) {
+		foreach my $eth ( @eth_bad ) {
+			_start_ethminer($eth);	
+		}
+	}
+	my %eth_bad = map{$_=>1} @eth_bad;
+	my @xmr_gpu=grep(  ! defined $eth_bad{$_}, @bad_gpus);
+	### @xmr_gpu
+	if ( scalar @xmr_gpu > 0) {
+		_start_gpuXMR(\@xmr_gpu);
+        }
+
+}
+
 
 sub _get_gpu_state {
 	my %return;
@@ -36,7 +106,7 @@ sub _get_gpu_state {
 	@results = grep(!/^#/, @results);   
 	### @results
 	close(RUN);
-	my @ethminer = grep(/ethminer/, @results);
+	my @ethminer = grep(/ethminer|ccminer/, @results);
 	my @gpu = grep(/X/, @results);
 	my %miner;
 	my %gpu;
@@ -46,6 +116,7 @@ sub _get_gpu_state {
 		$miner{$temps[1]}->{PID}=$temps[2]; 
 		$miner{$temps[1]}->{UTIL}=$temps[4]; 
 		$miner{$temps[1]}->{MEM}=$temps[5]; 
+		$miner{$temps[1]}->{NAME}=$temps[-1]; 
 	}
 	### %miner
 	foreach ( @gpu ) {
@@ -53,6 +124,17 @@ sub _get_gpu_state {
 		### @temps
 		$gpu{$temps[1]}=$temps[2];
 	}
+	my %threshold = (
+		'ccminer'=> {
+			'UTIL' => 50,
+			'MEM' => 10,
+		},
+		'ethminer'=> {
+			'UTIL' => 90,
+			'MEM' => 70,
+		},
+		
+	);
 
 	### %gpu 
 	# gpu     pid  type    sm   mem   enc   dec   command
@@ -63,21 +145,24 @@ sub _get_gpu_state {
 			$return{$gpu}='-1';
 		}
 		else {
-			if ( $miner{$gpu}->{UTIL} < 90 ) {
-				_print("GPU $gpu: PID $miner{$gpu}->{PID} stopped or hang($miner{$gpu}->{UTIL})");
-				$return{$gpu}= $miner{$gpu}->{PID};
+			my $gpu_threshold = $threshold{$miner{$gpu}->{NAME}}->{UTIL};
+			my $mem_threshold = $threshold{$miner{$gpu}->{NAME}}->{MEM};
+
+					if ( $miner{$gpu}->{UTIL} < $gpu_threshold ) {
+						_print("GPU $gpu: $miner{$gpu}->{NAME} -> PID $miner{$gpu}->{PID} stopped or hang($miner{$gpu}->{UTIL})");
+						$return{$gpu}= $miner{$gpu}->{PID};
+					}
+					elsif ( $miner{$gpu}->{MEM} < $mem_threshold) {
+						_print("GPU $gpu: $miner{$gpu}->{NAME} -> PID $miner{$gpu}->{PID} stopped or han{($miner{$gpu}->{MEM}) . Memory STOP");
+						$return{$gpu}= $miner{$gpu}->{PID};
+					}
+					else {
+						print "GPU $gpu: $miner{$gpu}->{NAME} -> PID $miner{$gpu}->{PID} util $miner{$gpu}->{UTIL}% ,MEM $miner{$gpu}->{MEM} \n";
+					}
 			}
-			elsif ( $miner{$gpu}->{MEM} < 90) {
-				_print("GPU $gpu: PID $miner{$gpu}->{PID} stopped or hang($miner{$gpu}->{MEM}) . Memory STOP");
-				$return{$gpu}= $miner{$gpu}->{PID};
-			}
-			else {
-				print "GPU $gpu: PID $miner{$gpu}->{PID} util $miner{$gpu}->{UTIL}% ,MEM $miner{$gpu}->{MEM} \n";
-			}
+			
 		}
-		
-	}
-	return \%return;
+		return \%return;
 }
 
 
@@ -99,15 +184,27 @@ sub _kill_ethminer {
 
 sub _start_ethminer  {
 	my $input = shift;
-	my $worker = shift;
-	my $cmd = "/opt/miners/ethminer/ethminer -F http://127.0.0.1:8080/$worker -U --dag-load-mode sequential --cl-global-work 8192 --farm-recheck 200 --cuda-parallel-hash 4 --cuda-devices $input 2>/var/run/miner.$input.output &";	
+	my $cmd = "/opt/miners/ethminer/ethminer -F http://127.0.0.1:8080/$worker -U --dag-load-mode sequential --cl-global-work 8192 --farm-recheck 200 --cuda-parallel-hash 4 --cuda-devices $input >/var/run/miner.$input.output 2>&1 &";	
 	_print("start ethminer for $input ...");
 	print "$cmd\n";
 	exec($cmd ) or _print("fail to exec $!");
-	sleep 10;
-	
-
+	#sleep 10;
 }
+sub _start_gpuXMR  {
+# `pwd`/ccminer -a cryptonight -o stratum+tcp://pool.supportxmr.com:7777 -u 45pwYiBt5vvWm6KgsAkGkrZztC7NthMbEMMvgUGD6SitK7YDbPgFvJS2mNiW4Fx4Pb2oncinP92A1j7uVxHdjCmKHQpPjHH -p pandaE1 -d 0
+	my $ref_input = shift;
+	
+	my @gpus = @$ref_input;
+	my $gpu = join(',',@gpus);
+	my $cmd = "/opt/miners/ccminer/ccminer -a cryptonight -o $xmr_pool -u $xmr -p $worker -d $gpu 2>/tmp/miner.xmr.output 2>&1";	
+	_print("start GPU Miner XMR for $gpu ...");
+	print "$cmd\n";
+	system("$cmd &") or _print("fail to exec $!");
+	#sleep 10;
+}
+
+
+
 
 sub _get_worker {
 	my $c_host = hostname;
@@ -133,3 +230,26 @@ sub _print {
 	close(OUT);
 }
 
+sub _check_cpu_miner {
+	my $worker =shift;
+        my $cmd = 'ps -ef |grep  minerd';
+	_print("Checking CPU miner  ...");	
+        open (RUN,"$cmd|");
+        my @results = <RUN>;
+        @results = grep(/$worker/, @results);
+	close(RUN);
+	### @results
+	if ( scalar @results > 0 ) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+sub _start_cpu_miner {
+        my $cmd = "$dir/cpuminer/minerd -a cryptonight -o $xmr_pool -u $xmr.$cpu_worker -p x &";
+        _print("start CPU miner for XMR ...");
+        print "$cmd\n";
+        exec($cmd ) or _print("fail to exec $!");
+}
